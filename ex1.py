caa8626c69e18e52415d8addb5e0e38b90f34c3b
Saksham Singh <55451173+SakshamSingh-v2@users.noreply.github.com>
2020-10-25 13:14:06 +0530

    Assignment uploaded by Saksham

---
+++ ex1.py
@@ -0,0 +1,22 @@
+print("I will now count my chickens:")
+
+print("Hens", 25 + 30/6)
+print("Roosters", 100 - 25 * 3 % 4)
+
+print("3 +2 + 1 - 5 + 4 % 2 - 1 / 4 + 6")
+print(3 +2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+print(3 + 2 < 5 -7)
+
+print("what is 3 + 2?", 3+2)
+print("what is 5 - 7?", 5-7)
+
+print("Oh, that's why it's false!")
+
+print("How about some more??")
+
+print("5>-2", 5>-2)
+print("is 5 greater than or equal to -2??", 5>=-2)
+
+print("well this code was a nonsense!!")
